8498fce3d8bf55b8c7f141f767b5df49720344e8
Lloyd565 <samsr472@gmail.com>
2026-04-17 16:15:47 +0700

    feat : implement card base class

--- src/model/cards/Card.cpp
+++ src/model/cards/Card.cpp
@@ -4,13 +4,17 @@ using namespace std;
 
 Card::Card() {}
 
-Card::Card(const string& name, const string& description) {}
+Card::Card(const string& name, const string& description) : name(name), description(description) {}
 
-Card::Card(const Card& other) {}
+Card::Card(const Card& other) : name(other.name), description(other.description){}
 
 Card::~Card() {}
 
 Card& Card::operator=(const Card& other) {
+    if(this != &other){
+        name = other.name;
+        description = other.description;
+    }
     return *this;
 }
 
