c3fc0bd26c7946f1d0f5669fab74c77cce793674
Lloyd565 <samsr472@gmail.com>
2026-04-18 23:35:28 +0700

    feat : add CommunityChestCard child classes implementation

--- src/model/cards/DoctorFeeCard.cpp
+++ src/model/cards/DoctorFeeCard.cpp
@@ -1,17 +1,55 @@
 #include "model/cards/DoctorFeeCard.hpp"
+#include "model/Game.hpp"
+#include "model/Player.hpp"
+
+#include <iostream>
 
 using namespace std;
 
-DoctorFeeCard::DoctorFeeCard() {}
+DoctorFeeCard::DoctorFeeCard()
+    : CommunityChestCard(), fee(0) {}
 
-DoctorFeeCard::DoctorFeeCard(const string& name, const string& description, int fee) {}
+DoctorFeeCard::DoctorFeeCard(const string& name, const string& description, int fee)
+    : CommunityChestCard(name, description), fee(fee) {}
 
-DoctorFeeCard::DoctorFeeCard(const DoctorFeeCard& other) {}
+DoctorFeeCard::DoctorFeeCard(const DoctorFeeCard& other)
+    : CommunityChestCard(other), fee(other.fee) {}
 
 DoctorFeeCard::~DoctorFeeCard() {}
 
 DoctorFeeCard& DoctorFeeCard::operator=(const DoctorFeeCard& other) {
+    if (this != &other) {
+        CommunityChestCard::operator=(other);
+        fee = other.fee;
+    }
     return *this;
 }
 
-void DoctorFeeCard::apply(Game& game, Player& player) {}
+void DoctorFeeCard::apply(Game& game, Player& player) {
+    cout << "Kartu: \"Biaya dokter. Bayar M" << fee << ".\"" << endl;
+
+    if (player.getMoney() >= fee) {
+        player.deductMoney(fee);
+        cout << "Kamu membayar M" << fee << " ke Bank. "
+             << "Sisa Uang = M" << player.getMoney() << "." << endl;
+
+        game.getLogManager().addLog(
+            game.getTurnManager().getCurrentTurn(),
+            player.getUsername(),
+            "KARTU",
+            "Membayar biaya dokter M" + to_string(fee) + " ke Bank"
+        );
+    } else {
+        cout << "Kamu tidak mampu membayar biaya dokter! (M" << fee << ")" << endl;
+        cout << "Uang kamu saat ini: M" << player.getMoney() << endl;
+
+        game.getLogManager().addLog(
+            game.getTurnManager().getCurrentTurn(),
+            player.getUsername(),
+            "BANGKRUT",
+            "Tidak mampu membayar biaya dokter M" + to_string(fee)
+        );
+
+        game.getBankruptcyManager().beginBankruptcySession(game, player, nullptr, fee, true);
+    }
+}
