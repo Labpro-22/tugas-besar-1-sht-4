1f053ed619b22bf1126f86a83106119a96931250
Lloyd565 <samsr472@gmail.com>
2026-04-18 23:36:34 +0700

    feat : add InstantCard

--- src/model/cards/InstantCard.cpp
+++ src/model/cards/InstantCard.cpp
@@ -2,14 +2,20 @@
 
 using namespace std;
 
-InstantCard::InstantCard() {}
+InstantCard::InstantCard()
+    : Card() {}
 
-InstantCard::InstantCard(const string& name, const string& description) {}
+InstantCard::InstantCard(const string& name, const string& description)
+    : Card(name, description) {}
 
-InstantCard::InstantCard(const InstantCard& other) {}
+InstantCard::InstantCard(const InstantCard& other)
+    : Card(other) {}
 
 InstantCard::~InstantCard() {}
 
 InstantCard& InstantCard::operator=(const InstantCard& other) {
+    if (this != &other) {
+        Card::operator=(other);
+    }
     return *this;
 }
