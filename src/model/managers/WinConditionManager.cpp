132862d1dbfc8d0afb9693a33984d8cc311e8ee0
alivovue <cubapetuking@gmail.com>
2026-04-22 17:14:05 +0700

    refactor : added more details in determine winners

--- src/model/managers/WinConditionManager.cpp
+++ src/model/managers/WinConditionManager.cpp
@@ -8,11 +8,14 @@ using namespace std;
 // not used?
 WinConditionManager::WinConditionManager() {}
 // not used?
-WinConditionManager::WinConditionManager(const WinConditionManager& other) {}
+WinConditionManager::WinConditionManager(const WinConditionManager& other) {
+    (void) other;
+}
 // not used?
 WinConditionManager::~WinConditionManager() {}
 // not used?
 WinConditionManager& WinConditionManager::operator=(const WinConditionManager& other) {
+    (void) other;
     return *this;
 }
 
@@ -20,9 +23,7 @@ bool WinConditionManager::isGameOver(const Game& game) const {
     if (onlyOnePlayerLeft(game)) {
         return true;
     }
-    // cek turn belum ditambahin
-    // if (game.getTurn() >= game.getMaxTurns()) {
-    return true;
+    return reachedMaxTurn(game);
 }
 
 bool WinConditionManager::reachedMaxTurn(const Game& game) const {
@@ -41,9 +42,34 @@ bool WinConditionManager::onlyOnePlayerLeft(const Game& game) const {
 
 vector<Player*> WinConditionManager::determineWinners(Game& game) const {
     vector<Player*> winners;
-    for (const Player& player : game.getPlayers()) {
-        if (!player.isBankrupt()) {
-            winners.push_back(const_cast<Player*>(&player));
+    int bestMoney = 0;
+    int bestProperties = 0;
+    int bestCards = 0;
+    bool hasCandidate = false;
+
+    for (Player& player : game.getPlayers()) {
+        if (player.isBankrupt()) {
+            continue;
+        }
+
+        const int money = player.getMoney();
+        const int properties = game.getPropertyManager().countOwnedProperties(game.getBoard(), player);
+        const int cards = player.countCards();
+        const bool better = !hasCandidate ||
+                            money > bestMoney ||
+                            (money == bestMoney && properties > bestProperties) ||
+                            (money == bestMoney && properties == bestProperties && cards > bestCards);
+
+        if (better) {
+            winners.clear();
+            bestMoney = money;
+            bestProperties = properties;
+            bestCards = cards;
+            hasCandidate = true;
+        }
+
+        if (money == bestMoney && properties == bestProperties && cards == bestCards) {
+            winners.push_back(&player);
         }
     }
     return winners;
