8cac08cfd03a5ab1da9d0b4d41c4ee9bda6ccfb4
alivovue <cubapetuking@gmail.com>
2026-04-22 17:12:16 +0700

    feat : implemented bankruptcy manager

--- src/model/managers/BankruptcyManager.cpp
+++ src/model/managers/BankruptcyManager.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-BankruptcyManager::BankruptcyManager() {}
+BankruptcyManager::BankruptcyManager()
+    : requiredAmount(0),
+      creditor(nullptr),
+      debtToBank(true),
+      bankruptcyActive(false),
+      debtor(nullptr) {}
 
 BankruptcyManager::BankruptcyManager(
     int requiredAmount,
@@ -22,10 +27,7 @@ BankruptcyManager::BankruptcyManager(const BankruptcyManager& other) {
     }
 }
 
-BankruptcyManager::~BankruptcyManager() {
-    delete creditor;
-    delete debtor;
-}
+BankruptcyManager::~BankruptcyManager() {}
 
 BankruptcyManager& BankruptcyManager::operator=(const BankruptcyManager& other) {
     if (this != &other) {
@@ -46,29 +48,70 @@ bool BankruptcyManager::canCoverDebt(const Player& player, int amount) const {
 }
 
 int BankruptcyManager::estimateLiquidationValue(const Player& player) const {
-    return 0;
+    return player.getMoney();
 }
 
-void BankruptcyManager::processDebtToPlayer(Player& debtor, Player& creditor, int amount) {}
+void BankruptcyManager::processDebtToPlayer(Player& debtor, Player& creditor, int amount) {
+    if (debtor.getMoney() >= amount) {
+        debtor -= amount;
+        creditor += amount;
+        return;
+    }
+    beginBankruptcySession(debtor, &creditor, amount, false);
+}
 
-void BankruptcyManager::processDebtToBank(Player& debtor, int amount) {}
+void BankruptcyManager::processDebtToBank(Player& debtor, int amount) {
+    if (debtor.getMoney() >= amount) {
+        debtor -= amount;
+        return;
+    }
+    beginBankruptcySession(debtor, nullptr, amount, true);
+}
 
 bool BankruptcyManager::isDebtCovered(const Player& player) const {
-    return false;
+    return player.getMoney() >= requiredAmount;
 }
 
 bool BankruptcyManager::hasLiquidationOptions(const Player& player) const {
-    return false;
+    return player.getMoney() > 0;
 }
 
 bool BankruptcyManager::isBankruptcyActive() const {
     return bankruptcyActive;
 }
 
-void BankruptcyManager::settleDebt(Player& debtor) {}
+void BankruptcyManager::settleDebt(Player& debtor) {
+    if (requiredAmount <= 0 || debtor.getMoney() < requiredAmount) {
+        return;
+    }
 
-void BankruptcyManager::declareBankrupt(Player& debtor, Player* creditor) {}
+    debtor -= requiredAmount;
+    if (!debtToBank && creditor != nullptr) {
+        *creditor += requiredAmount;
+    }
+    bankruptcyActive = false;
+    requiredAmount = 0;
+    this->debtor = nullptr;
+    this->creditor = nullptr;
+}
 
-void BankruptcyManager::beginBankruptcySession(Player& player, Player* creditor, int amount, bool debtToBank) {}
+void BankruptcyManager::declareBankrupt(Player& debtor, Player* creditor) {
+    (void) creditor;
+    debtor.setBankrupt(true);
+    bankruptcyActive = true;
+}
 
-void BankruptcyManager::resolveLiquidationAction(Player& player, const string& action) {}
+void BankruptcyManager::beginBankruptcySession(Player& player, Player* creditor, int amount, bool debtToBank) {
+    this->debtor = &player;
+    this->creditor = creditor;
+    this->requiredAmount = amount;
+    this->debtToBank = debtToBank;
+    this->bankruptcyActive = true;
+}
+
+void BankruptcyManager::resolveLiquidationAction(Player& player, const string& action) {
+    (void) action;
+    if (isDebtCovered(player)) {
+        settleDebt(player);
+    }
+}
